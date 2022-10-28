6314d93e9c7b8da34e1a8aae956a3ab39cc96f5e
ADJI-DOMORAK Alexia <alexia.adji@gmail.com>
2022-10-28 15:18:37 +0200

    back:2

--- routes/favorite.js
+++ routes/favorite.js
@@ -8,7 +8,23 @@ const API_KEY = process.env.API_KEY;
 
 // Route lieu afin d'avoir les coordonnées d'une ville 
 router.get('/city', (req, res) => {
-    fetch(`https://api.opentripmap.com/0.1/en/places/geoname?name=Lille&country=fr&apikey=${API_KEY}`)
+      const city = req.body.city;
+      const country = req.body.country;
+    fetch(`https://api.opentripmap.com/0.1/en/places/geoname?name=${city}&country=${country}&apikey=${API_KEY}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+       res.json(data);
+    });
+})
+
+// Route Food 
+router.get('/foods', (req, res) => {
+      const lat_min = req.body.latitude;
+      const lat_max = lat_min + 1;
+      const lon_min = req.body.longitude;
+      const lon_max = lon_min + 1;
+    fetch(`https://api.opentripmap.com/0.1/en/places/bbox?lon_min=${lon_min}&lon_max=${lon_max}&lat_min=${lat_min}&lat_max=${lat_max}&kinds=foods&rate=1&format=json&apikey=${API_KEY}`)
     .then(response => response.json())
     .then(data => {
       console.log(data);
@@ -16,15 +32,14 @@ router.get('/city', (req, res) => {
     });
 })
 
-// Route Food 
-router.get('/foods', (req, res) => {
-    fetch(`https://api.opentripmap.com/0.1/en/places/bbox?lon_min=1.3488&lon_max=2.5&lat_min=47.85341&lat_max=50&kinds=foods&rate=1&format=json&apikey=${API_KEY}`)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      return data;
-    });
-})
+// Route Visit
+router.get("/visits", (req, res) => {
+  fetch(
+    "https://api.opentripmap.com/0.1/en/places/bbox?lon_min=3.05858&lon_max=4&lat_min=50.63297&lat_max=51&src_geom=wikidata&src_attr=wikidata&format=json&apikey=5ae2e3f221c38a28845f05b6998fe19bd4efc90feefb6f24d37a5b15"
+  ).then(resp => resp.json())
+  .then(data => 
+    res.json({visits: data}))
+});
 
 
 module.exports = router;
