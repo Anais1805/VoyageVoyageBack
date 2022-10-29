13d2f41b16ab9482cba7165d5a3c1c3b59b64285
Anais1805 <111952504+Anais1805@users.noreply.github.com>
2022-10-29 21:38:43 +0200

    route foods

--- routes/favorite.js
+++ routes/favorite.js
@@ -21,9 +21,9 @@ router.get('/:city/:country', (req, res) => {
 // Route Food 
 router.get('/foods/:longitude/:latitude', (req, res) => {
       const lat_min = req.params.latitude;
-      const latitudemax = req.params.latitude + 1;
+      const latitudemax = req.params.latitude + 0.000001;
       const lon_min = req.params.longitude;
-      const longitudemax = req.params.longitude + 1;
+      const longitudemax = req.params.longitude + 0.000001;
     fetch(`https://api.opentripmap.com/0.1/en/places/bbox?lon_min=${req.params.longitude}&lon_max=${longitudemax}&lat_min=${req.params.latitude}&lat_max=${latitudemax}&kinds=foods&rate=1&format=json&apikey=${API_KEY}`)
     .then(response => response.json())
     .then(data => {
@@ -32,15 +32,56 @@ router.get('/foods/:longitude/:latitude', (req, res) => {
     });
 })
 
+router.get('/:longitude/:latitude', (req, res) => {
+  const lat_min = req.params.latitude;
+  const latitudemax = (req.params.latitude) + 1
+  const lon_min = req.params.longitude;
+  const longitudemax = (req.params.longitude) + 1
+fetch(`https://api.opentripmap.com/0.1/en/places/bbox?lon_min=${req.params.longitude}&lon_max=${longitudemax}&lat_min=${req.params.latitude}&lat_max=${latitudemax}&kinds=foods&rate=1&format=json&apikey=${API_KEY}`)
+.then(response => response.json())
+.then(data => {
+res.json({result: true, foods:data})
+ 
+});
+})
+
 // Route Visit
+
 router.get("/visits", (req, res) => {
+
   
-  fetch(
-    "https://api.opentripmap.com/0.1/en/places/bbox?lon_min=3.05858&lon_max=4&lat_min=50.63297&lat_max=51&src_geom=wikidata&src_attr=wikidata&format=json&apikey=5ae2e3f221c38a28845f05b6998fe19bd4efc90feefb6f24d37a5b15"
-  ).then(resp => resp.json())
-  .then(data => 
-    res.json({result: true, visits: data}))
+
+  const lat_min = req.body.latitude;
+  const lat_max = lat_min + 0.000001;
+  const lon_min = req.body.longitude;
+  const lon_max = lon_min + 0.000001;
+
+fetch(
+`https://api.opentripmap.com/0.1/en/places/bbox?lon_min=${lon_min}&lon_max=${lon_max}&lat_min=${lat_min}&lat_max=${lat_max}&kinds=historic&cultural&rate=1&format=json&apikey=${API_KEY}`
+).then(resp => resp.json())
+.then(data => 
+res.json({result: true, visits: data}))
 });
 
+// Route Randonnée
+router.get("/natural", (req, res) => {
+const lat_min = req.body.latitude;
+const lat_max = lat_min + 0.000001;
+const lon_min = req.body.longitude;
+const lon_max = lon_min + 0.000001;
+fetch(
+`https://api.opentripmap.com/0.1/en/places/bbox?lon_min=${lon_min}&lon_max=${lon_max}&lat_min=${lat_min}&lat_max=${lat_max}&kinds=natural&rate=1&format=json&apikey=${API_KEY}`
+).then(resp => resp.json())
+.then(data => 
+res.json({visits: data}))
+});
+
+router.get("/infos", (req, res) => {fetch(
+`https://api.opentripmap.com/0.1/en/places/xid/Q3552599?apikey=${API_KEY}`
+).then(resp => resp.json())
+.then(data => 
+res.json({infos: data}))
+});
 
 module.exports = router;
+
--- routes/foods.js
+++ routes/foods.js
@@ -5,16 +5,20 @@ const fetch = require('node-fetch');
 const API_KEY = process.env.API_KEY;
 
 
-router.get('/:longitude/:latitude', (req, res) => {
-    const lat_min = req.params.latitude;
-    const latitudemax = req.params.latitude + 1;
-    const lon_min = req.params.longitude;
-    const longitudemax = req.params.longitude + 1;
-  fetch(`https://api.opentripmap.com/0.1/en/places/bbox?lon_min=${req.params.longitude}&lon_max=${longitudemax}&lat_min=${req.params.latitude}&lat_max=${latitudemax}&kinds=foods&rate=1&format=json&apikey=${API_KEY}`)
+router.get('/:lonmin/:longitudemax/:latmin/:latitudemax', (req, res) => {
+    const latmin = req.params.latmin;
+    const latitudemax = req.params.latitudemax
+    const lonmin = req.params.lonmin;
+    const longitudemax = req.params.longitudemax
+    console.log('lonmax', longitudemax)
+    console.log('lonmin', lonmin)
+    console.log('latmax', latitudemax)
+    console.log( 'latmin',latmin)
+  fetch(`https://api.opentripmap.com/0.1/en/places/bbox?lon_min=${lonmin}&lon_max=${longitudemax}&lat_min=${latmin}&lat_max=${latitudemax}&kinds=foods&rate=1&format=json&apikey=${API_KEY}`)
   .then(response => response.json())
   .then(data => {
-    console.log({result: true, foods: data});
-    return data;
+  res.json({result: true, foods:data})
+   
   });
 })
 
