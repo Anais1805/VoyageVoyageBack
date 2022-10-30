107af871d77999df8e9e2bb3bbb3d66830212bf3
Anais1805 <111952504+Anais1805@users.noreply.github.com>
2022-10-30 10:10:31 +0100

    msj routes visits naturals infos

---
+++ routes/infos.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+const fetch = require('node-fetch');
+
+const API_KEY = process.env.API_KEY;
+
+
+router.get('/:xid', (req, res) => {
+    const xid = req.params.xid
+  fetch(`https://api.opentripmap.com/0.1/en/places/xid/${xid}?apikey=${API_KEY}`)
+  .then(response => response.json())
+  .then(data => {
+    if(data !== null){
+        res.json({result: true, infos: data})
+    }else {
+        res.json({result: false, error: 'No city found'})
+    }
+   
+  });
+})
+
+module.exports = router;
